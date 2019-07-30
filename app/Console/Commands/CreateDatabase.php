51a28275efbd9ab958215dd859ab8a7ffa8bf454
Samba24 <juniornjay@gmail.com>
2019-07-30 17:09:29 +0000

    Added make:database command

---
+++ app/Console/Commands/CreateDatabase.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class CreateDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:database {database}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a specified database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        return DB::connection('mysql')->statement('CREATE DATABASE '.$this->argument('database'));
+    }
+}
